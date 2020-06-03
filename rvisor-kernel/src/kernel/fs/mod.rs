a54056ec17358a4988f5a2506afc4f1bd55c74d6
root <dnailzb@outlook.com>
2020-06-03 10:06:35 +0000

    fs half completed

--- rvisor-kernel/src/kernel/fs/mod.rs
+++ rvisor-kernel/src/kernel/fs/mod.rs
@@ -2,7 +2,7 @@
 use linux_kernel_module::bindings;
 
 use spin::RwLock as RcuLock;
-use alloc::str::String;
+use alloc::string::String;
 use alloc::sync::Arc;
 use alloc::sync::Weak;
 use alloc::collections::linked_list::LinkedList;
@@ -14,15 +14,14 @@ pub mod open;
 
 struct INode {
     ino : u64,
-    dentry : Weak<Mutex<DEntry>>,
     mode : u64,
     uid : u64,
     gid : u64,
+    ismounted : u64,
+    mapped_to : String, // map to file at orig system
 }
 
-impl INode {
-    
-}
+impl INode {}
 
 struct DEntry {
     name : String,
@@ -32,10 +31,32 @@ struct DEntry {
 }
 
 impl DEntry {
-    pub fn new(name : String, parent : Weak<Mutex<DEntry>>) -> DEntry {
+    pub fn new(name : String, parent : Weak<Mutex<DEntry>>, ino : u64, mode : u64, uid : u64, gid : u64) -> DEntry {
         DEntry {
-            name : name,
-            inode : 
+            name: name,
+            inode: Arc::new(
+                Mutex::new(INode{
+                    ino: ino,
+                    mode: mode,
+                    uid: uid,
+                    gid: gid,
+                    ismounted: false,
+                })
+            ),
+            parent: parent,
+            child: LinkedList::new(),
         }
     }
+
+    pub fn createChild(name : String, parent : Weak<Mutex<DEntry>>, ino : u64, mode : u64, uid : u64, gid : u64) {
+        
+        self.child.push_back(
+            Arc::new(
+                Mutex::new(
+                    new(name, parent, ino, mode, uid, gid)
+                )
+            )
+        )
+    }
+
 }
